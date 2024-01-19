0efef7f871b7d7d1842bd00f627ab55304a7ef6f
mposolda <mposolda@gmail.com>
2024-01-19 10:34:59 +0100

    Simple button working
    Signed-off-by: mposolda <mposolda@gmail.com>

---
+++ react-nextjs-tutorial/my-app/app/MyApp1.tsx
@@ -0,0 +1,16 @@
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export function MyApp1() {
+  return (
+    <div>
+      <h1>Welcome to my app 1</h1>
+      <MyButton />
+    </div>
+  );
+}
--- react-nextjs-tutorial/my-app/app/page.tsx
+++ react-nextjs-tutorial/my-app/app/page.tsx
@@ -1,3 +1,13 @@
+import { MyApp1 } from "./MyApp1";
+
 export default function Page() {
-  return <h1>Hello, my next.js!</h1>
+  return (
+    <div>
+      <h1>Hello, my next.js!</h1>
+      <hr />
+
+      <MyApp1 />
+      <hr />
+    </div>
+  )
 }
