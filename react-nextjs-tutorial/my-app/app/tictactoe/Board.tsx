cecd2e4e6f99aff1df2a202bc4ad1a95bdf3ee58
mposolda <mposolda@gmail.com>
2024-01-19 18:30:02 +0100

    Winner computing
    Signed-off-by: mposolda <mposolda@gmail.com>

--- react-nextjs-tutorial/my-app/app/tictactoe/Board.tsx
+++ react-nextjs-tutorial/my-app/app/tictactoe/Board.tsx
@@ -9,7 +9,30 @@ export function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [history, setHistory] = useState([]);
 
+  function checkLines(i1: number, i2: number, i3: number, squaress) {
+    // console.log("checking " + i1 + ", " + i2 + ", " + i3);
+    // console.log(squaress[i1] + " " + squaress[i2] + " " + squaress[i3]);
+    if (squaress[i1] === 'O' && squaress[i2] === 'O' && squaress[i3] === 'O') return 'O';
+    if (squaress[i1] === 'X' && squaress[i2] === 'X' && squaress[i3] === 'X') return 'X';
+    return null;
+  }
+  function isWinner(squaress) {
+    let lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (var i=0; i<lines.length ; i++) {
+      var indexes = lines[i];
+      var c = checkLines(indexes[0], indexes[1], indexes[2], squaress);
+      if (c == 'O' || c == 'X') return c;
+    }
+    return null;
+  }
+
   function handleClick(buttonIndex) {
+    var winner = isWinner(squares);
+    if (winner != null) {
+      console.log("Winner is already " + winner);
+      return;
+    }
+
     console.log("Clicked button" + buttonIndex);
 
     // I need to explicitly call "slice()" to create copy of the array. I cannot use the original array as otherwise stuff won't be re-rendered!!!
@@ -22,6 +45,11 @@ export function Board() {
 
     newSquares[buttonIndex] = charr;
     setSquares(newSquares);
+
+    winner = isWinner(newSquares);
+    if (winner != null) {
+      console.log("Winner is " + winner);
+    }
 
     var newHistory = history.slice();
     newHistory.push(newSquares);
