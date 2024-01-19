5f9c624caf70072c477b8fdf471fdbf365c8513f
mposolda <mposolda@gmail.com>
2024-01-19 17:45:03 +0100

    Basic playing works
    Signed-off-by: mposolda <mposolda@gmail.com>

--- react-nextjs-tutorial/my-app/app/tictactoe/Board.tsx
+++ react-nextjs-tutorial/my-app/app/tictactoe/Board.tsx
@@ -5,37 +5,33 @@ import "./styles.css";
 
 export function Board() {
 
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(buttonIndex) {
+    console.log("Clicked button" + buttonIndex);
+
+    // I need to explicitly call "slice()" to create copy of the array. I cannot use the original array as otherwise stuff won't be re-rendered!!!
+    var newSquares = squares.slice();
+    newSquares[buttonIndex] = 'X';
+    setSquares(newSquares);
+  }
+
   return (
     <>
       <div className="board-row">
-        <MyButton btnIndex="1" />
-        <MyButton btnIndex="2" />
-        <MyButton btnIndex="3" />
-      </div>
-      <div className="board-row">
-        <MyButton btnIndex="4" />
-        <MyButton btnIndex="5" />
-        <MyButton btnIndex="6" />
-      </div>
-      <div className="board-row">
-        <MyButton btnIndex="7" />
-        <MyButton btnIndex="8" />
-        <MyButton btnIndex="9" />
+        <MyButton btnIndex={1} value={squares[0]} handleClickFnc={() => handleClick(0)} />
+        <MyButton btnIndex={2} value={squares[1]} handleClickFnc={() => handleClick(1)} />
+        <MyButton btnIndex={3} value={squares[2]} handleClickFnc={() => handleClick(2)} />
       </div>
     </>
   );
 }
 
-function MyButton({btnIndex}) {
+function MyButton({btnIndex, value, handleClickFnc}) {
 
-  const [value, setValue] = useState(btnIndex);
-
-  function handleClick() {
-    console.log("Clicked" + btnIndex);
-    setValue("X");
-  }
+  // const [value, setValue] = useState(btnIndex);
 
   return (
-    <button className="square" onClick={handleClick}>{value}</button>
+    <button className="square" onClick={handleClickFnc}>{value}</button>
   )
 }
