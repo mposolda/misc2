2ed6e475420baae21996e22f4aae55ad1bed47b7
mposolda <mposolda@gmail.com>
2024-03-01 09:13:47 +0100

    Input Filter - filtering
    Signed-off-by: mposolda <mposolda@gmail.com>

--- react-nextjs-tutorial/my-app/app/thinking-in-react/Thinking.tsx
+++ react-nextjs-tutorial/my-app/app/thinking-in-react/Thinking.tsx
@@ -17,24 +17,46 @@ function getData() {
 }
 
 export function ThinkingInReact() {
+
+  const [inputFilter, setInputFilter] = useState("");
+
+  function onFilterChange() {
+    let val = document.getElementById("input-filter").value;
+    console.log("On filter change: " + val);
+    setInputFilter(val);
+  }
+
   return (
     <>
       <h3>Thinking In React - tutorial</h3>
-      <VegTable type="Fruits" />
+      <InputFilter newValue={inputFilter} onFilterChangeHandler={() => onFilterChange()} />
       <br />
       <br />
-      <VegTable type="Vegetables" />
+      <VegTable type="Fruits" inputFilter={inputFilter} />
+      <br />
+      <VegTable type="Vegetables" inputFilter={inputFilter} />
+    </>
+  );
+}
+
+function InputFilter({newValue, onFilterChangeHandler}) {
+  return (
+    <>
+      <b>Input filter: </b><input type="text" id="input-filter" name="input-filter" onInput={onFilterChangeHandler} />
     </>
   );
 }
 
 // type is "Fruits" or "Vegetables"
-function VegTable({type}) {
+function VegTable({type, inputFilter}) {
   let data = getData();
   let rows = [];
+  // This is re-rendered when the state is changed (so for instance when filter is changed)
+  console.log("Re-render of the table with inputFilter: " + inputFilter);
   for (let i=0 ; i<data.length ; i++) {
     let currentDataRow = data[i];
-    if (currentDataRow.category === type) {
+    // Starts with filtering
+    if (currentDataRow.category === type && data[i].name.toLowerCase().startsWith(inputFilter.toLowerCase())) {
       rows.push((
         <Row key={i} rowData={data[i]} />
       ));
