e3f0b99c09b9a842fd0f96cdb9e23be32cf96524
mposolda <mposolda@gmail.com>
2024-03-01 09:32:55 +0100

    Checkbox works
    Signed-off-by: mposolda <mposolda@gmail.com>

--- react-nextjs-tutorial/my-app/app/thinking-in-react/Thinking.tsx
+++ react-nextjs-tutorial/my-app/app/thinking-in-react/Thinking.tsx
@@ -19,6 +19,7 @@ function getData() {
 export function ThinkingInReact() {
 
   const [inputFilter, setInputFilter] = useState("");
+  const [stockCheckboxChecked, setStockCheckboxChecked] = useState(false);
 
   function onFilterChange() {
     let val = document.getElementById("input-filter").value;
@@ -26,37 +27,66 @@ export function ThinkingInReact() {
     setInputFilter(val);
   }
 
+  function onStockCheckboxChange() {
+    let checked = document.getElementById("only-stocked-checkbox").checked;
+    console.log("On checkbox change. Checked: " + checked);
+    setStockCheckboxChecked(checked);
+  }
+
   return (
     <>
       <h3>Thinking In React - tutorial</h3>
-      <InputFilter newValue={inputFilter} onFilterChangeHandler={() => onFilterChange()} />
+      <table>
+        <tbody>
+        <tr>
+          <InputFilter onFilterChangeHandler={() => onFilterChange()} />
+        </tr>
+        <tr>
+          <StockFilterCheckbox onCheckboxChangeHandler={() => onStockCheckboxChange()} />
+        </tr>
+        </tbody>
+      </table>
       <br />
       <br />
-      <VegTable type="Fruits" inputFilter={inputFilter} />
       <br />
-      <VegTable type="Vegetables" inputFilter={inputFilter} />
+      <VegTable type="Fruits" inputFilter={inputFilter} stockCheckboxChecked={stockCheckboxChecked} />
+      <br />
+      <VegTable type="Vegetables" inputFilter={inputFilter} stockCheckboxChecked={stockCheckboxChecked} />
     </>
   );
 }
 
-function InputFilter({newValue, onFilterChangeHandler}) {
+function InputFilter({onFilterChangeHandler}) {
   return (
     <>
-      <b>Input filter: </b><input type="text" id="input-filter" name="input-filter" onInput={onFilterChangeHandler} />
+      <td><b>Input filter: </b></td>
+      <td><input type="text" id="input-filter" name="input-filter" onInput={onFilterChangeHandler} /></td>
+    </>
+  );
+}
+
+function StockFilterCheckbox({onCheckboxChangeHandler}) {
+  return (
+    <>
+      <td><b>Only stocked: </b></td>
+      <td><input type="checkbox" id="only-stocked-checkbox" name="only-stocked-checkbox" onChange={onCheckboxChangeHandler} /></td>
     </>
   );
 }
 
 // type is "Fruits" or "Vegetables"
-function VegTable({type, inputFilter}) {
+function VegTable({type, inputFilter, stockCheckboxChecked}) {
   let data = getData();
   let rows = [];
   // This is re-rendered when the state is changed (so for instance when filter is changed)
-  console.log("Re-render of the table with inputFilter: " + inputFilter);
+  console.log("Re-render of the table with inputFilter: " + inputFilter + ", stockCheckbox: " + stockCheckboxChecked);
   for (let i=0 ; i<data.length ; i++) {
     let currentDataRow = data[i];
     // Starts with filtering
-    if (currentDataRow.category === type && data[i].name.toLowerCase().startsWith(inputFilter.toLowerCase())) {
+    if (currentDataRow.category === type
+         && data[i].name.toLowerCase().startsWith(inputFilter.toLowerCase())
+         && (!stockCheckboxChecked || data[i].stocked)
+       ) {
       rows.push((
         <Row key={i} rowData={data[i]} />
       ));
